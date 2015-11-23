73cfad89101a39a7bca1a2fa60f02f258de9669b
David Stoup <david.stoup@kitware.com>
2015-11-23 15:42:58 -0500

    Fix bug in cmake path.

--- dashcv01_vxl.sh
+++ dashcv01_vxl.sh
@@ -2,4 +2,4 @@
 
 /Users/kitware/cmake/install/v3.0.0-rc4/bin/ctest -S dashcv01_vxl_release.cmake -VV >& logs/release.log
 /Users/kitware/cmake/install/v3.0.0-rc4/bin/ctest -S dashcv01_vxl_debug.cmake -VV >& logs/debug.log
-/usr/bin/ctest -S dashcv01_vxl_experimental.cmake -VV >& logs/experimental.log
+/Users/kitware/cmake/install/v3.0.0-rc4/bin/ctest -S dashcv01_vxl_experimental.cmake -VV >& logs/experimental.log
