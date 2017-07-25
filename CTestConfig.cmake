95d0ec894a952934de50e10b18b492b28ba17469
Adam Kogan <adam.kogan@kitware.com>
2017-07-25 14:20:33 -0400

    added stl custom warning exceptions

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -6,3 +6,14 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "open.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=vxl")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
+message("IN CONFIG SCRIPT")
+
+set(CTEST_CUSTOM_WARNING_EXCEPTION
+  ${CTEST_CUSTOM_WARNING_EXCEPTION}
+
+  "stl_construct.h:.*: warning: Forming reference to null pointer"
+  ".*stl_uninitialized.*: warning: Forming reference to null pointer.*"
+  "stl_vector.h:771:9: warning: Returning null reference"
+)
+
