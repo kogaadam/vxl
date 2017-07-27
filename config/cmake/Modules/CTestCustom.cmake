65df3f72a05d09d1d2d932a5ea98deb77d53e76d
Adam Kogan <adam.kogan@kitware.com>
2017-07-27 15:59:11 -0400

    removed error exclusions, changed warning limit to 1000

--- config/cmake/Modules/CTestCustom.cmake
+++ config/cmake/Modules/CTestCustom.cmake
@@ -5,25 +5,10 @@
 
 # default is to report 50 errors and 50 warnings
 set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_ERRORS 500)
-set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 500)
+set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 1000)
 
 set(CTEST_CUSTOM_MAXIMUM_FAILED_TEST_OUTPUT_SIZE 102400)
 
 # default is 1024
 set(CTEST_CUSTOM_MAXIMUM_PASSED_TEST_OUTPUT_SIZE 102400)
 
-# message("NEW IN CONFIG SCRIPT")
-
-# set(CTEST_CUSTOM_WARNING_EXCEPTION
- # ${CTEST_CUSTOM_WARNING_EXCEPTION}
-
-  # "[0-9,]+ warnings? generated."
-
-  # ".*stl_construct.h:.*: warning: Forming reference to null pointer.*"
-  # ".*stl_uninitialized.h:.*: warning: Forming reference to null pointer.*" 
-
-  # ".*stl_vector.h:.*: warning: Returning null reference.*"
-  # ".*clipper/clipper.cpp:3954:9: warning: Value stored to 'k' is never read.*"
-  # ".*clipper.cpp:4339:27: warning: Division by zero.*"
-
-# )
